d90a241ed42f7f162554cdee5214ecea9cbf3003
abulechu <luis.tabiro10@gmail.com>
2015-07-31 19:40:05 -0400

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {bailong2
-	"bailong2": 1
+	"camperomon": 1
 };
 
 // crash guard - write errors to log file instead of crashing
